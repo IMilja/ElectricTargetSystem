fe3ac58f45b321996ca45618032e9a2604d734d0
IvanM <ivan.miljancic@gmail.com>
2018-05-08 14:11:54 +0200

    Inital commit

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import cv2
+import numpy as np
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main_page():
+    return "Test"
 
 
 if __name__ == '__main__':
